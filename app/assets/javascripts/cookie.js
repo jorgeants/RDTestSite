a518a97259bcc951e5aad019b8b1a5a738762899
Jorge Antônio <jorgeants@gmail.com>
2017-06-18 01:39:34 -0300

    Return with url for development access

--- app/assets/javascripts/cookie.js
+++ app/assets/javascripts/cookie.js
@@ -1,7 +1,9 @@
 var CookieHandler = (function() {
 
-  var url = 'https://rdtestapplication.herokuapp.com/contacts/cookie';
-  var urlForSendCurretPage = 'https://rdtestapplication.herokuapp.com/contacts/access';
+  // var url = 'https://rdtestapplication.herokuapp.com/contacts/cookie';
+  // var urlForSendCurretPage = 'https://rdtestapplication.herokuapp.com/contacts/access';
+  var url = 'http://localhost:3000/contacts/cookie';
+  var urlForSendCurretPage = 'http://localhost:3001/contacts/access';
   var cookieName = '_rdtestsite.cookie';
 
   var init = function() {
